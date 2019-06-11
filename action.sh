8b126c3b2ae7649b86413505c630be685278d366
Simon Li <orpheus+devel@gmail.com>
2019-06-11 22:32:09 +0100

    action wrapper sets git user and email

--- action.sh
+++ action.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
-# GitHub Action may use a different home, need to reinitialise helm
+# GitHub Action:
+# - Set git author
+# - Uses different home, need to reinitialise helm
 set -eu
+
+if [ -n "$GITHUB_ACTOR" ]; then
+    git config --global user.name "$GITHUB_ACTOR"
+    git config --global user.email "$GITHUB_ACTOR@users.noreply.github.com"
+fi
 
 helm init --client-only
 exec chartpress.py "$@"
